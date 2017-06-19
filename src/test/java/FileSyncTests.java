38bef9bdb843f62aaf7121c096f863670eb4da27
SergiiShcherbakov <strannikmail@gmail.com>
2017-06-19 23:34:01 +0300

    Change structure of the program. Now it has only one class with inner classes.

--- src/test/java/FileSyncTests.java
+++ src/test/java/FileSyncTests.java
@@ -1,4 +1,6 @@
 import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
@@ -6,26 +8,67 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+import static org.junit.Assert.assertTrue;
+
 /**
  * Created by Sergii Shcherbakov on 19.06.2017.
  */
 public class FileSyncTests {
 
     String soursDir = "H:\\Juja\\projects\\synch\\Test\\Test3";
-    String destDir = "H:\\Juja\\projects\\synch\\Test\\Test2";
+    String destDir = "H:\\Juja\\projects\\synch\\Test\\Test4";
 
-    @Test
+    @Before
     public void setData() throws IOException {
         Path sours = Paths.get(soursDir);
-        Files.createDirectories(sours, null );
+        Files.createDirectories(sours );
         sours = Paths.get(soursDir + "\\test1");
-        Files.createDirectories(sours, null );
+        Files.createDirectories(sours );
         sours = Paths.get(soursDir + "\\test2");
-        Files.createDirectories(sours, null );
+        Files.createDirectories(sours);
         sours = Paths.get(soursDir + "\\test2\\test.txt");
-        Files.createFile(sours, null );
+        Files.createFile(sours );
     }
 
+    @Test
+    public void TestWithoutDestDir() throws IOException {
+        //given
+        Path dest = Paths.get(destDir);
+        Files.deleteIfExists(dest);
+        //when
+        FileSync.main(new String[]{soursDir, destDir});
+        //then
+        assertTrue( Files.exists(dest));
+        dest = Paths.get(destDir + "\\test2\\test.txt");
+        assertTrue( Files.exists(dest ));
+        dest = Paths.get(destDir + "\\test2");
+        assertTrue( Files.exists(dest ));
+        dest = Paths.get(destDir + "\\test1");
+        assertTrue( Files.exists(dest ));
+    }
+
+
+    @After
+    public void deleteDataFromTests() throws IOException {
+        //delete sours files
+        Path sours = Paths.get(soursDir + "\\test2\\test.txt");
+        Files.deleteIfExists(sours );
+        sours = Paths.get(soursDir + "\\test1");
+        Files.deleteIfExists(sours );
+        sours = Paths.get(soursDir + "\\test2");
+        Files.deleteIfExists(sours);
+        sours = Paths.get(soursDir);
+        Files.deleteIfExists(sours );
+        //delete dest files
+        Path dest = Paths.get(destDir + "\\test2\\test.txt");
+        Files.deleteIfExists(dest );
+        dest = Paths.get(destDir + "\\test2");
+        Files.deleteIfExists(dest );
+        dest = Paths.get(destDir + "\\test1");
+        Files.deleteIfExists(dest );
+        dest = Paths.get(destDir );
+        Files.deleteIfExists(dest );
+    }
 
 
 }
